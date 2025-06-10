bff51c5d2f4eebeb9b25f397af9fbc871d0d543f
syabbes <159195901+syabbes@users.noreply.github.com>
2025-06-10 16:38:24 +0200

    db toegevoegd en initialisatie

--- program.py
+++ program.py
@@ -1,6 +1,16 @@
 import RPi.GPIO as GPIO
 import time
-import cs50
-print("hello, world!")
-for i in range(6):
-  print("string")
+from cs50 import SQL
+import pygame
+
+db = cs50.SQL("sqlite:///highscores.db")
+GPIO.setmode(GPIO.BCM)  # Use bcm
+
+#initialiseer pygame
+pygame.init()
+screen = pygame.display.set_mode((1024, 600))
+pygame.display.set_caption("Hello, Pygame")
+# main
+
+# cleanup pins
+GPIO.cleanup()
